73ce67c2225d105b96e47deb92929267de24054b
Willie Lima <williecadete@gmail.com>
2023-06-26 22:22:59 +0100

    Add initial project structure

---
+++ src/mytoolbox/__init__.py
@@ -0,0 +1,4 @@
+"""Top-level package for mytoolbox."""
+
+__version__ = "0.0.0"
+__app_name__ = "MyToolbox"
---
+++ src/mytoolbox/cli.py
@@ -0,0 +1,29 @@
+import typer
+from typing import Optional
+
+from mytoolbox import __app_name__, __version__
+
+app = typer.Typer(add_completion=False)
+
+
+def _version_callback(value: bool) -> None:
+    if value:
+        typer.echo(f"{__app_name__} v{__version__}")
+        raise typer.Exit()
+
+
+@app.callback()
+def main(
+    version: Optional[bool] = typer.Option(
+        None,
+        "--version",
+        "-v",
+        help="Show the application's version and exit.",
+        callback=_version_callback,
+        is_eager=True,
+    )
+) -> None:
+    return
+
+
+app()
