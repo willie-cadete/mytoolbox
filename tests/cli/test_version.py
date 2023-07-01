b9cf5226e361ab522eb2271b30c105deefff57b9
Willie Lima <w@willielima.me>
2023-07-01 19:45:43 +0100

    Update pre-commit configuration for mypy and bandit

--- tests/cli/test_version.py
+++ tests/cli/test_version.py
@@ -5,7 +5,7 @@ from mytoolbox import __app_name__, __version__, cli
 runner = CliRunner()
 
 
-def test_version():
+def test_version() -> None:
     result = runner.invoke(cli.app, "--version")
     assert result.exit_code == 0
     assert f"{__app_name__} v{__version__}\n" in result.stdout
